24d3604704b91794a01a7681a52c4526b90dc577
Alice Newton <alice.e.newton@gmail.com>
2013-07-03 19:08:47 +0100

    static

--- outfits/urls.py
+++ outfits/urls.py
@@ -16,5 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('items.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
+              #static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
